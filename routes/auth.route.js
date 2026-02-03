3247c5ec2ce795724c4e7cb93032432690fa4a3d
renda <renda.kouachi@gmail.com>
2026-02-03 18:01:10 +0100

    fix email verification

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,13 @@
-// Dans routes/auth.route.js
 import express from 'express';
-// On utilise signin et signup car c'est comme ça qu'ils sont nommés dans ton controller
-import { signin, signup, signout } from '../controllers/auth.controller.js'; 
+// Ajoute verifyEmail (ou le nom exact défini dans ton controller) ici :
+import { signin, signup, signout, verifyEmail } from '../controllers/auth.controller.js'; 
 
 const router = express.Router();
 
-router.post('/register', signup); // On lie l'URL /register à la fonction signup
-router.post('/login', signin);    // On lie l'URL /login à la fonction signin
+router.post('/register', signup); 
+router.post('/login', signin);    
 router.post('/logout', signout);
+
+router.get('/verify-email/:token', verifyEmail);
 
 export default router;
